922e88bf7c71cff029fe92b78d6cce9f8a469fff
Michael Xiourouppa <michaelxiour@hotmail.com>
2025-02-20 18:43:41 +0200

    set up scripts and redirect on npm start

--- odysseyex-app/src/App.js
+++ odysseyex-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
@@ -13,6 +13,7 @@ const Layout = () => {
       {!isLoginPage && <Sidebar />} 
       <div className={`content ${isLoginPage ? "no-sidebar" : "with-sidebar"}`}>
         <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={ <ProtectedRoute> <Home /> </ProtectedRoute>} />  { /*ProtectedRoute is used to not allow a user that is not logged in to navigate to welcome or products page */}
           <Route path="/products" element={ <ProtectedRoute> <Products /> </ProtectedRoute>} />
